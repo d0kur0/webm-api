81b64aa216b4f0aefd167f6f4b52f44bc4ac0728
d0kur0 <dev.dokuro@gmail.com>
2020-01-28 21:08:23 +0300

    Fix configStruct

--- helpers/grabberSchemaHelper/types.go
+++ helpers/grabberSchemaHelper/types.go
@@ -3,6 +3,6 @@ package grabberSchemaHelper
 import "github.com/d0kur0/webm-grabber/sources/types"
 
 type configStruct struct {
-	AllowedExtensions []types.AllowedExtensions
-	GrabberSchema     map[string][]string
+	AllowedExtensions types.AllowedExtensions
+	GrabberSchema     map[string][]types.Board
 }
