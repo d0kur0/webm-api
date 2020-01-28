b93c03281ff3f54b39abc9c5b45afa62406d0490
d0kur0 <dev.dokuro@gmail.com>
2020-01-28 21:08:05 +0300

    Add parsing config file, and create if not exists with default settings

--- helpers/grabberSchemaHelper/GrabberSchema.go
+++ helpers/grabberSchemaHelper/GrabberSchema.go
@@ -1,7 +1,12 @@
 package grabberSchemaHelper
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"os"
+
+	"github.com/pkg/errors"
 
 	"github.com/d0kur0/webm-grabber/sources/fourChannel"
 	"github.com/d0kur0/webm-grabber/sources/twoChannel"
@@ -10,31 +15,78 @@ import (
 )
 
 type grabberSchema struct {
-	defaultGrabberSchema     []types.GrabberSchema
-	defaultAllowedExtensions types.AllowedExtensions
-	configFilePath           string
+	grabberSchema     []types.GrabberSchema
+	allowedExtensions types.AllowedExtensions
+	configFilePath    string
 }
 
-func (schema *grabberSchema) parseConfig() (grabberSchema []types.GrabberSchema, err error) {
-	// TODO: Доделать
-	if _, err := os.Stat(schema.configFilePath); os.IsNotExist(err) {
+func (schema *grabberSchema) createConfigFromDefault() error {
+	var grabberSchema = make(map[string][]types.Board, len(schema.grabberSchema))
 
+	for _, schema := range schema.grabberSchema {
+		grabberSchema[schema.Vendor.VendorName()] = schema.Boards
+	}
+
+	var configData = configStruct{
+		AllowedExtensions: schema.allowedExtensions,
+		GrabberSchema:     grabberSchema,
+	}
+
+	jsonBytes, err := json.MarshalIndent(configData, "", "\t")
+	if err != nil {
+		return errors.Wrap(err, "Marshaling config error")
+	}
+
+	if err := ioutil.WriteFile(schema.configFilePath, jsonBytes, 0644); err != nil {
+		return errors.Wrap(err, "Write in config data file error")
+	}
+
+	return nil
+}
+
+func (schema *grabberSchema) parseConfig() (err error) {
+	if _, err := os.Stat(schema.configFilePath); os.IsNotExist(err) {
+		return schema.createConfigFromDefault()
+	}
+
+	jsonData, err := ioutil.ReadFile(schema.configFilePath)
+	if err != nil {
+		return errors.Wrap(err, "Read config file error")
+	}
+
+	var configData configStruct
+	if err := json.Unmarshal(jsonData, &configData); err != nil {
+		return errors.Wrap(err, "Unmarshal config file error")
+	}
+
+	schema.allowedExtensions = configData.AllowedExtensions
+	for schemaIndex, schemaData := range schema.grabberSchema {
+		if _, vendorsExists := configData.GrabberSchema[schemaData.Vendor.VendorName()]; !vendorsExists {
+			schema.grabberSchema[schemaIndex] = schema.grabberSchema[len(schema.grabberSchema)-1]
+			schema.grabberSchema[len(schema.grabberSchema)-1] = types.GrabberSchema{}
+			schema.grabberSchema = schema.grabberSchema[:len(schema.grabberSchema)-1]
+
+			continue
+		}
+
+		schema.grabberSchema[schemaIndex].Boards = configData.GrabberSchema[schemaData.Vendor.VendorName()]
 	}
 
 	return
 }
 
 func (schema *grabberSchema) Get() []types.GrabberSchema {
-	return schema.defaultGrabberSchema
+	return schema.grabberSchema
 }
 
 func Make() (schema *grabberSchema) {
 	schema = &grabberSchema{configFilePath: "config.json"}
 
-	schema.defaultAllowedExtensions = types.AllowedExtensions{".webm", ".mp4"}
-	schema.defaultGrabberSchema = []types.GrabberSchema{
+	// Default settings
+	schema.allowedExtensions = types.AllowedExtensions{".webm", ".mp4"}
+	schema.grabberSchema = []types.GrabberSchema{
 		{
-			twoChannel.Make(schema.defaultAllowedExtensions),
+			twoChannel.Make(schema.allowedExtensions),
 			[]types.Board{
 				{"b", "Бред"},
 				{"vg", "Видео Игры"},
@@ -48,7 +100,7 @@ func Make() (schema *grabberSchema) {
 			},
 		},
 		{
-			fourChannel.Make(schema.defaultAllowedExtensions),
+			fourChannel.Make(schema.allowedExtensions),
 			[]types.Board{
 				{"a", "Anime & Manga"},
 				{"c", "Anime/Cute"},
@@ -66,6 +118,11 @@ func Make() (schema *grabberSchema) {
 				{"d", "Hentai/Alternative"},
 			},
 		},
+	}
+
+	// Parse config file
+	if err := schema.parseConfig(); err != nil {
+		log.Println(err)
 	}
 
 	return
