f7fd19eef8aa2f5d6d206cb02abad5b4f0173d0a
d0kur0 <dev.dokuro@gmail.com>
2020-02-06 01:51:45 +0300

    Add accessMiddleware, fix CORS requests for files/getByStrust

--- ServerEntry.go
+++ ServerEntry.go
@@ -24,12 +24,27 @@ func main() {
 	go grabberTask.Start()
 
 	router := mux.NewRouter()
+	router.Use(accessControlMiddleware)
 	router.HandleFunc("/schema/get", schemaHttpHandler.GetSchema).Methods("GET")
-	router.HandleFunc("/files/getByStruct", filesHttpHandler.GetFilesByStruct).Methods("POST")
+	router.HandleFunc("/files/getByStruct", filesHttpHandler.GetFilesByStruct).Methods("POST", "OPTIONS")
 	router.HandleFunc("/files/getAll", filesHttpHandler.GetAll).Methods("GET")
 
 	if err := http.ListenAndServe(":"+port, router); err != nil {
 		log.Println("Starting server failed: ", err)
 	}
 	log.Println("Server started at port:", port)
+}
+
+func accessControlMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS,PUT")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
