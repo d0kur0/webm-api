b5f25f333bb6ba87c3f0bc956e78677698263a68
d0kur0 <dev.dokuro@gmail.com>
2020-03-07 20:54:22 +0300

    Add Gzip handler

--- ServerEntry.go
+++ ServerEntry.go
@@ -1,68 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/pkg/errors"
-
+	"github.com/d0kur0/webm-api/helpers/settingsManager"
 	"github.com/d0kur0/webm-api/httpHandlers/filesHttpHandler"
 	"github.com/d0kur0/webm-api/httpHandlers/schemaHttpHandler"
 	"github.com/d0kur0/webm-api/tasks/grabberTask"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
-
-type ServerConfig struct {
-	Port           int
-	UpdateInterval uint64
-}
-
-func parseServerConfig() (config ServerConfig, err error) {
-	const configFilePath = "serverConfig.json"
-	const defaultPort = 3500
-	const defaultUpdateInterval = 10
-
-	config = ServerConfig{
-		Port:           defaultPort,
-		UpdateInterval: defaultUpdateInterval,
-	}
-
-	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
-		jsonBytes, err := json.MarshalIndent(config, "", "\t")
-		if err != nil {
-			return config, errors.Wrap(err, fmt.Sprintf("Marshaling config error (%s)", configFilePath))
-		}
-
-		if err := ioutil.WriteFile(configFilePath, jsonBytes, 0644); err != nil {
-			return config, errors.Wrap(err, fmt.Sprintf("Write in config data file error (%s)", configFilePath))
-		}
-	} else {
-		jsonData, err := ioutil.ReadFile(configFilePath)
-		if err != nil {
-			return config, errors.Wrap(err, fmt.Sprintf("Read config file error (%s)", configFilePath))
-		}
-
-		if err := json.Unmarshal(jsonData, &config); err != nil {
-			return config, errors.Wrap(err, fmt.Sprintf("Unmarshal config file error (%s)", configFilePath))
-		}
-	}
-
-	return
-}
 
 func main() {
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Println("Recovered in f", r)
+			log.Println("Recovered in f", r)
 		}
 	}()
 
-	config, err := parseServerConfig()
+	config, err := settingsManager.Parse()
 	if err != nil {
 		log.Println(err)
 		return
@@ -77,7 +35,7 @@ func main() {
 	router.HandleFunc("/files/getAll", filesHttpHandler.GetAll).Methods("GET")
 
 	log.Println("Server started at port:", config.Port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", config.Port), router); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", config.Port), handlers.CompressHandler(router)); err != nil {
 		log.Println("Starting server failed: ", err)
 	}
 }
