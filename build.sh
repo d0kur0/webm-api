36c4a2ea819f9de9273948691709468943b72943
d0kur0 <dev.dokuro@gmail.com>
2020-04-02 18:55:03 +0800

    Add go get before build

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ rm -rf ./build
 echo "Build for windows..."
 export GOOS=windows
 export GOARCH=386
+go get -v
 go build -o ./build/webm-api.windows.exe ServerEntry.go
 
 echo "Build for linux..."
 export GOOS=linux
 export GOARCH=386
+go get -v
 go build -o ./build/webm-api.linux ServerEntry.go
