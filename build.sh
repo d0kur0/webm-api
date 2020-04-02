d312d6d581f78d0fffd1b304acf622cc9cf6d0b9
d0kur0 <dev.dokuro@gmail.com>
2020-04-02 18:48:44 +0800

    Bash script for build api for linux and windows

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Clear old builds"
+rm -rf ./build
+
+echo "Build for windows..."
+export GOOS=windows
+export GOARCH=386
+go build -o ./build/webm-api.windows.exe ServerEntry.go
+
+echo "Build for linux..."
+export GOOS=linux
+export GOARCH=386
+go build -o ./build/webm-api.linux ServerEntry.go
